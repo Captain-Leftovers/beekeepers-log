85a397d8bf17728209baa4aa66ed3bc4866ec91d
Captain-Leftovers <beeondweb@gmail.com>
2024-04-15 08:09:31 +1000

    Add environment variable initialization and error handling in main.go

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func init() {
 	fmt.Println("Initialization function")
+	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DB_CONNECTION  := os.Getenv("DB_CONNECTION")
+	if DB_CONNECTION == "" {
+		log.Fatal("DB_CONNECTION is not set in .env file")
+	}	
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("PORT is not set in .env file")
+	}
+
+
 }
 
 func main() {
---
+++ routes.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func addRoutes(
+	mux *http.ServeMux,
+) {
+
+}
